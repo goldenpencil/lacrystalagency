f32a7f89c6e27e08c983987844d0f3b4a85615f8
goldenpencil <georgeisaac603@gmail.com>
2019-11-15 08:31:20 +0100

    mac commits

--- js/ajax.js
+++ js/ajax.js
@@ -1,29 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
-</head>
-<body>
-  <!-- modify this form HTML and place wherever you want your form -->
-<form id="my-form"
-action="https://formspree.io/mknwwwjv"
-method="POST"
->
-<label>Name:</label>
-<input type="name" name="name" />
-<label>Email:</label>
-<input type="email" name="email" />
-<label>Message:</label>
-<input type="text" name="message" />
-<button id="my-form-button">Submit</button>
-<p id="my-form-status"></p>
-</form>
-
-<!-- Place this script at the end of the body tag -->
-
 <script>
 window.addEventListener("DOMContentLoaded", function() {
 
@@ -71,5 +45,3 @@ function ajax(method, url, data, success, error) {
   xhr.send(data);
 }
 </script>
-</body>
-</html>
